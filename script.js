1e3bee1f9a3dc0e7c52868f5e3048a4b27281ab7
shahkewal <44388696+shahkewal@users.noreply.github.com>
2019-10-26 14:13:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var mysql = require('mysql');
+const app = express();
+var connection = mysql.createPool({
+connectionLimit: 50,
+host: 'localhost',
+user: 'root',
+password: '',
+database: 'counsel'
+});
+
+app.get('/',function(req, resp){
+
+    connection.getConnection(function(error, tempCont){
+            if(!!error) {
+                tempCont.release();
+                console.log('Error');
+            } else {
+                console.log('Connected');
+
+                tempCont.query("SELECT * FROM counsellor", function(error, rows, fields){
+                    tempCont.release();
+                    if(!!error){
+                        console.log('Error in the query');
+                    } else{
+                        resp.json(rows);
+                    }
+                });
+            }
+
+    });
+});
+
+app.listen(1337);
